ad2a42ecc08fe221e6ca5601412d77cf70495f20
Thomas Provan <tcprovan@ucdavis.edu>
2018-03-16 13:58:38 -0700

    More framework more notes

--- Anonymous ML/KNearestSystem.py
+++ Anonymous ML/KNearestSystem.py
@@ -16,6 +16,38 @@ class KNearestSystem:
         # It's been way too long since I wrote code. Way too long.
 
 
+    def train(data):
+        self.input = data
+        
+        
+        
+    def test(data):
+        # No need to save this, we're just going to be outputting results.
+
+"""
+
+Okay, let's examine a question. Does this need to be a class?
+Probably not, but my OO inclinations tell me to make it one XD
+
+Let's see... oh, okay, I have an okay reason to do it this way.
+
+We're going to be training 2 different machines with each type.
+Comparing them is going to be relevant, so having a system object
+for each run seems reasonable. We can then use other functions to
+compare the two.
+
+Hrm... Cross-fold. Where do / How do / Do we want to handle cross-fold?
+We haven't answered that question quite yet. Not sure how important that
+is yet, we can implement later.
+
+
+Secondary question: Do we want to have a MLSystem superclass?
+
+I'm not sure. We can compare all the anomaly system with each other.
+And if we had classes, then we could 
+But ultimately we're evaluating how these systems behave within themselves.
+
+"""
 
 
 """
