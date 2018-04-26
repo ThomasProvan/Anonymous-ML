0a23b877dd6524f387a4b8e0f42fb7d8044a79d5
Erin <mozdye@gmail.com>
2018-04-26 14:37:36 -0700

    softflowd implemented

--- runner.py
+++ runner.py
@@ -1,6 +1,13 @@
 import subprocess, os
+import sys
 
 directory = '/Users/ErinMozdy/Desktop/runnerfiles'
 for filename in os.listdir(directory):
 	subprocess.call(['tcpdump','-r',directory + '/' + filename,'tcp','-w','/Users/ErinMozdy/Desktop/runnertcp/'+filename])
 	subprocess.call(['tcpdump','-r',directory + '/' + filename,'udp','-w','/Users/ErinMozdy/Desktop/runnerudp/'+filename])
+
+
+sys.stdout = open('netflowoutput', 'w')
+second_directory = '/Users/ErinMozdy/Desktop/runnertcp'
+for filename in os.listdir(second_directory):
+	subprocess.call(['softflowd','-r',directory + '/' + filename])
