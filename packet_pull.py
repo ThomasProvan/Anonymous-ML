6bf2981f9dea0440f5e866e174c4c354c4fe675b
Erin <mozdye@gmail.com>
2018-05-03 13:54:19 -0700

    packet list create

---
+++ packet_pull.py
@@ -0,0 +1,11 @@
+import logging
+logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
+from scapy.all import *
+
+packet_list = []
+directory = 'runnerfiles'
+for filename in os.listdir(directory):
+	packets = rdpcap(directory + '/' + filename)
+	for packet in packets:
+		packet_list.append(packet)
+		num_packets += 1
